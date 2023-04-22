6dfcb5a5b37aae6e8fdbd2ec5d490832cf765eb3
Thomas Volden <thvo@itu.dk>
2023-04-22 22:00:06 +0200

    Add project files.

---
+++ ConsoleEventDispatchDecorator.cs
@@ -0,0 +1,22 @@
+﻿using dk.itu.game.msc.cgol.Distribution;
+
+namespace CGOL_webapp
+{
+    internal class ConsoleEventDispatchDecorator : IEventDispatcher
+    {
+        private readonly IEventDispatcher dispatcher;
+        private readonly ConsoleWriter consoleWriter;
+
+        public ConsoleEventDispatchDecorator(IEventDispatcher dispatcher, ConsoleWriter consoleWriter)
+        {
+            this.dispatcher = dispatcher ?? throw new ArgumentNullException(nameof(dispatcher));
+            this.consoleWriter = consoleWriter ?? throw new ArgumentNullException(nameof(consoleWriter));
+        }
+
+        public async Task Dispatch(IEvent @event)
+        {
+            consoleWriter.Write(@event);
+            await dispatcher.Dispatch(@event);
+        }
+    }
+}
---
+++ ConsoleWriter.cs
@@ -0,0 +1,21 @@
+﻿using dk.itu.game.msc.cgol.Distribution;
+
+namespace CGOL_webapp
+{
+    internal class ConsoleWriter
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine($"{DateTime.Now}: {message}");
+        }
+
+        public void Write(IEvent @event)
+        {
+            if (@event.GetType().ToString() != @event.ToString())
+                Console.WriteLine($"{DateTime.Now} [Event][{@event.GetType().Name}]: {@event}");
+            else
+                Console.WriteLine($"{DateTime.Now} [Event][{@event.GetType().Name}]");
+        }
+    }
+
+}
